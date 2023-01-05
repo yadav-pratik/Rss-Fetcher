3a8608b3c824b1f18b5e18e1e0364330c4c447a5
pratik <yadavpratik8806@gmail.com>
2023-01-06 02:03:50 +0530

    client : added UserSelection component

--- client/src/components/Articles/ArticlesList.js
+++ client/src/components/Articles/ArticlesList.js
@@ -1,9 +1,13 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 
 const ArticlesList = (props) => {
+    const [source, setSource] = useState('Times of India')
     return (
         <div>
             ArticlesList
+            <form>
+
+            </form>
         </div>
     )
 }
--- client/src/components/Body.js
+++ client/src/components/Body.js
@@ -1,14 +1,11 @@
 import React from "react"
-import { Route } from 'react-router-dom'
 
 import ArticlesList from "./Articles/ArticlesList"
 
 const Body = (props) => {
     return (
         <div className="container">
-            <h2>Body</h2>
-
-            <Route path="/articles/list" component={ArticlesList}/>
+            <ArticlesList />
         </div>
     )
 }
