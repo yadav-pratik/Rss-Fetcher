8a3e2f02ca536c644fdc06abcef71ef1681dd6dd
pratik <yadavpratik8806@gmail.com>
2023-01-06 02:43:04 +0530

    client : added dropdown for category and source

--- client/src/components/Articles/UserSelection.js
+++ client/src/components/Articles/UserSelection.js
@@ -1,9 +1,53 @@
 import React from "react"
+import { useSelector, useDispatch} from 'react-redux'
 
 const UserSelection = (props) => {
+    const categories = ['latest', 'india', 'technology', 'cricket']
+
+    const dispatch = useDispatch()
+
+    const category = useSelector((state)=>{
+        return state.category
+    })
+    const source = useSelector((state)=>{
+        return state.source
+    })
+
+    const handleChange = (e) => {
+        const name = e.target.name
+        if(name === 'source'){
+            dispatch()
+        } else if(name === 'category'){
+            dispatch()
+        }
+    }
+
     return (
         <div>
-            
+            <form>
+                <label>Change the Source of Articles</label>
+                <select 
+                    value={source} 
+                    onChange={handleChange}
+                    name="source"
+                >
+                    <option value="TOI">Times of India</option>
+                    <option value="NDTV">NDTV</option>
+                </select>
+                <label>Change the Category</label>
+                <select 
+                    value={category}
+                    onChange={handleChange}
+                    name="category"
+                >
+                    {categories.map((cat , i) => {
+                        return <option
+                            key={i}
+                            value={cat}
+                        >{cat.charAt(0).toUpperCase()+cat.slice(1)}</option>
+                    })}
+                </select>
+            </form>
         </div>
     )
 }
--- client/src/components/Body.js
+++ client/src/components/Body.js
@@ -1,10 +1,12 @@
 import React from "react"
 
 import ArticlesList from "./Articles/ArticlesList"
+import UserSelection from "./Articles/UserSelection"
 
 const Body = (props) => {
     return (
         <div className="container">
+            <UserSelection />
             <ArticlesList />
         </div>
     )
