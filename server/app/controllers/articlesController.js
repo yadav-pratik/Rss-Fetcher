eca150bb546427fc6e1e3fb998cc8fe226e0a918
pratik <yadavpratik8806@gmail.com>
2023-01-06 00:10:00 +0530

    server : added api and controller for articles

--- server/app/controllers/articlesController.js
+++ server/app/controllers/articlesController.js
@@ -2,4 +2,14 @@ const Article = require('../models/article')
 
 articlesController = {}
 
+articlesController.list = async (req, res) => {
+    const {source, category, page, posts} = req.query
+    try {
+        const articles = await Article.find({source : source, category : category}).sort({date_published : -1}).skip(10 * (Number(page)-1)).limit(Number(posts))
+        res.json(articles)
+    } catch (error) {
+        res.json(error)
+    }
+}
+
 module.exports = articlesController
--- server/config/routes.js
+++ server/config/routes.js
@@ -3,6 +3,7 @@ const express = require('express')
 const authenticateUser = require('../app/middlewares/authentication')
 
 const usersController = require('../app/controllers/usersController')
+const articlesController = require('../app/controllers/articlesController')
 
 const router = express.Router()
 
@@ -11,5 +12,9 @@ const router = express.Router()
 router.post('/api/users/register', usersController.register)
 router.post('/api/users/login',usersController.login)
 router.get('/api/users/account', authenticateUser, usersController.show)
+
+//articles API
+
+router.get('/api/articles/list', articlesController.list)
 
 module.exports = router
