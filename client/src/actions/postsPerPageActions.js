04ad97d6bddf23856fb039dd9864df9d168eaf24
pratik <yadavpratik8806@gmail.com>
2023-01-06 03:52:37 +0530

    client : changing postsPerPage value from dropdown from PaginationComp

---
+++ client/src/actions/postsPerPageActions.js
@@ -0,0 +1,6 @@
+export const setPostsPerPage = (postsPerPage) => {
+    return {
+        type : 'SET_POSTS_PER_PAGE',
+        payload : postsPerPage
+    }
+}
--- client/src/components/Articles/PaginationComp.js
+++ client/src/components/Articles/PaginationComp.js
@@ -3,12 +3,17 @@ import { Pagination } from 'react-bootstrap'
 import { useSelector, useDispatch } from "react-redux"
 
 import { setPageNo } from "../../actions/pageNoActions"
+import { setPostsPerPage } from "../../actions/postsPerPageActions"
 
 const PaginationComp = (props) => {
     const dispatch = useDispatch()
 
     const pageNo = useSelector((state)=> {
         return state.pageNo
+    })
+
+    const postsPerPage = useSelector((state)=> {
+        return state.postsPerPage
     })
     
     let items = []
@@ -28,7 +33,15 @@ const PaginationComp = (props) => {
             <Pagination>{items}</Pagination>
             <form>
                 <label>Articles per page</label>
-                {/* <select value={} onChange={()=>dispatch()}></select> */}
+                <select 
+                    value={postsPerPage} 
+                    onChange={(e)=>dispatch(setPostsPerPage(e.target.value))}
+                >
+                    <option value="5">5</option>
+                    <option value="10">10</option>
+                    <option value="15">15</option>
+                    <option value="20">20</option>
+                </select>
             </form>
         </div>
     )
