963e4ced565576ee350782808c7f67189d21b79b
pratik <yadavpratik8806@gmail.com>
2023-01-06 22:07:42 +0530

    client : moved PostsPerPage dropdown to UserSelection and styled userselection

--- client/src/components/Articles/PaginationComp.js
+++ client/src/components/Articles/PaginationComp.js
@@ -11,10 +11,6 @@ const PaginationComp = (props) => {
     const pageNo = useSelector((state)=> {
         return state.pageNo
     })
-
-    const postsPerPage = useSelector((state)=> {
-        return state.postsPerPage
-    })
     
     let items = []
     for (let i = 1; i <= 5; i++) {
@@ -31,18 +27,6 @@ const PaginationComp = (props) => {
     return (
         <div>
             <Pagination>{items}</Pagination>
-            <form>
-                <label>Articles per page</label>
-                <select 
-                    value={postsPerPage} 
-                    onChange={(e)=>dispatch(setPostsPerPage(e.target.value))}
-                >
-                    <option value="5">5</option>
-                    <option value="10">10</option>
-                    <option value="15">15</option>
-                    <option value="20">20</option>
-                </select>
-            </form>
         </div>
     )
 }
--- client/src/components/Articles/UserSelection.js
+++ client/src/components/Articles/UserSelection.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch} from 'react-redux'
 
 import { setSource } from "../../actions/sourceActions"
 import { setCategory } from "../../actions/categoryActions"
+import { setPostsPerPage } from "../../actions/postsPerPageActions"
 
 const UserSelection = (props) => {
     const categories = ['latest', 'india', 'technology', 'cricket']
@@ -15,6 +16,9 @@ const UserSelection = (props) => {
     const source = useSelector((state)=>{
         return state.source
     })
+    const postsPerPage = useSelector((state)=> {
+        return state.postsPerPage
+    })
 
     const handleChange = (e) => {
         const name = e.target.name
@@ -26,32 +30,53 @@ const UserSelection = (props) => {
     }
 
     return (
-        <div>
-            <h2>You are seeing the {category.charAt(0).toUpperCase() + category.slice(1)} articles from {source}</h2>
-            <form>
-                <label>Change the Source of Articles</label>
-                <select 
-                    value={source} 
-                    onChange={handleChange}
-                    name="source"
-                >
-                    <option value="TOI">Times of India</option>
-                    <option value="NDTV">NDTV</option>
-                </select>
-                <label>Change the Category</label>
-                <select 
-                    value={category}
-                    onChange={handleChange}
-                    name="category"
-                >
-                    {categories.map((cat , i) => {
-                        return <option
-                            key={i}
-                            value={cat}
-                        >{cat.charAt(0).toUpperCase()+cat.slice(1)}</option>
-                    })}
-                </select>
-            </form>
+        <div className="mt-3 mb-3">
+            <h2 className="text-center">You are seeing the {category.charAt(0).toUpperCase() + category.slice(1)} articles from {source}</h2>
+            <div className="d-flex justify-content-between mt-3">
+                <form>
+                    <label>Articles per page</label>
+                    <select 
+                        className="form-control"
+                        value={postsPerPage} 
+                        onChange={(e)=>dispatch(setPostsPerPage(e.target.value))}
+                    >
+                        <option value="5">5</option>
+                        <option value="10">10</option>
+                        <option value="15">15</option>
+                        <option value="20">20</option>
+                    </select>
+                </form>
+                <form className="d-flex justify-content-end">
+                    <div className="form-group  me-5">
+                        <label>Change the Source of Articles</label>
+                        <select 
+                            className="form-control"
+                            value={source} 
+                            onChange={handleChange}
+                            name="source"
+                        >
+                            <option value="TOI">Times of India</option>
+                            <option value="NDTV">NDTV</option>
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label>Change the Category</label>
+                        <select 
+                            className="form-control"
+                            value={category}
+                            onChange={handleChange}
+                            name="category"
+                        >
+                            {categories.map((cat , i) => {
+                                return <option
+                                    key={i}
+                                    value={cat}
+                                >{cat.charAt(0).toUpperCase()+cat.slice(1)}</option>
+                            })}
+                        </select>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
