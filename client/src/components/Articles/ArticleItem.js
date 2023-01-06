6064be90ff345110fda738e265f5ebfdfc611d1f
pratik <yadavpratik8806@gmail.com>
2023-01-06 15:39:37 +0530

    client : added ArticleItem as cards

--- client/src/components/Articles/ArticleItem.js
+++ client/src/components/Articles/ArticleItem.js
@@ -1,10 +1,22 @@
 import React from "react"
 
 const ArticleItem = (props) => {
-    const {title, thumbnail, description} = props
+    const {title, thumbnail, description, pubDate, link} = props
+
+    const backgroundStyle = {
+        height : '200px', 
+        backgroundImage : `url(${thumbnail})`, 
+        backgroundSize : 'cover', 
+        backgroundPosition : "0% 50%" 
+    }
     return (
-        <div>
-            title : {title}
+        <div className="card bg-dark text-white border-dark mt-4 shadow" style={backgroundStyle}>
+            <div className="card-img-overlay">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
+                <p className="card-text">Published : {pubDate}</p>
+                <span><a target="_blank" href={link}>View Full Article {`>>`}</a></span>
+            </div>
         </div>
     )
 }
