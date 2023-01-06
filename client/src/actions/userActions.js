c2d2af387913a4397922341e18223c4fc84e5f5a
pratik <yadavpratik8806@gmail.com>
2023-01-06 18:22:00 +0530

    client : added sweetalert as a helper function and normalAlert as a reusable function

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,21 +1,23 @@
 import axios from 'axios'
 
+import { normalAlert } from '../sweetAlerts/sweetAlerts'
+
 export const startLoginUser =  (formData, clearAndDispatch) => {
     return (
         async () => {
             try {
                 const { data } = await axios.post('http://localhost:3040/api/users/login',formData)
                 if(data.hasOwnProperty('notice')){
-                    alert(data.notice, 'error')
+                    normalAlert(data.notice, 'error')
                 } else if(data.hasOwnProperty('errors') || data.hasOwnProperty('message')){
-                    alert(data.message, 'error')
+                    normalAlert(data.message, 'error')
                 } else if(data.hasOwnProperty('token')){
-                    alert('Login Successful', 'success')
+                    normalAlert('Login Successful', 'success')
                     localStorage.setItem('token',data.token)
                     clearAndDispatch()
                 }
             } catch (error) {
-                alert(error.message, 'error')
+                normalAlert(error.message, 'error')
             }
         }
     )
